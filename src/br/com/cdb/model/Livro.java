45e654eebb66ccd5a32dff575d56516eb8961bf4
Jean silva <spereira.jean@outlook.com>
2023-06-15 20:27:27 -0300

    Refatoração de código

--- src/br/com/cdb/model/Livro.java
+++ src/br/com/cdb/model/Livro.java
@@ -1,41 +1,27 @@
-package br.com.cdb.model;
-
+//Crie uma classe chamada "Livro" com os seguintes atributos privados:
+//
+//título (String)
+//autor (String)
+//anoPublicacao (int)
+//preco (double)
+//A classe deve fornecer métodos públicos para acessar e modificar esses atributos (encapsulamento).
+//
+//Crie uma classe chamada "LivroFisico" que herda da classe "Livro". Essa classe representa um livro físico que pode ser vendido na livraria. Adicione um atributo privado extra na classe "LivroFisico":
+//
+//peso (double)
+//Além disso, adicione um método público chamado "calcularFrete" que recebe a distância em quilômetros e retorna o valor do frete com base no peso do livro. Considere que o valor do frete é de R$ 0,01 por grama por quilômetro.
+//
+//Crie uma classe chamada "LivroDigital" que herda da classe "Livro". Essa classe representa um livro digital que pode ser vendido na livraria. Adicione um atributo privado extra na classe "LivroDigital":
+//
+//formato (String)
 public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoDePublicacao;
+    private double preco;
 
-    private String title;
-    private String author;
-    private String identificationNumber;
-    private double price=3;
-
-    public String getTitle(){
-        return title;
+    public String getTitulo(){
+        return autor;
     }
-    public void setTitle(String title){
-        this.title = title;
-    }
-
-    public String getAuthor(){
-        return author;
-    }
-    public void setAuthor(String author){
-        this.author = author;
-    }
-
-    public String getIdentificationNumber(){
-        return identificationNumber;
-    }
-    public void setIdentificationNumber(String identificationNumber){
-        this.identificationNumber = identificationNumber;
-    }
-
-    public double getPrice(){
-        return price;
-    }
-    public void setPrice(double price){
-        this.price = price;
-    }
-
-    public void calculateTotalPrice(){
-        System.out.println("O preço do livro é de: R$"+price);
-    }
+    public void
 }
